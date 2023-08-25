927095bc4b565f818130c4926d421c95e50b5778
An Qi <a1n14q17i9@gmail.com>
2023-08-25 11:16:10 +0300

    Initial

---
+++ test.py
@@ -0,0 +1,30 @@
+intVar = 20
+floatVar = 3.2
+strVar = "Software 1"
+
+print(intVar)
+print()
+print(floatVar)
+print()
+print(strVar)
+# Casting the value from float to int
+result = int(intVar + floatVar)
+print(result)
+
+share0floan = 800.5 / 3
+print(share0floan)
+rndshare0floan = int(share0floan)
+print(rndshare0floan)
+
+num1 = int(input("Enter some value:\n "))
+num2 = int(input('Enter the second value: \n'))
+result = num1 + num2
+# result = int(num1) + int(num2)
+print("The result is: \t", result)
+
+name = input("What is your name: ")
+age = input("What is your age: ")
+print("Name: ", name, " Age: ", age)
+print(name, age)
+print(f"Name: {name} Age: {age}")
+
