931d6da3cb97b240840fd21eeba06082753e16e3
An Qi <a1n14q17i9@gmail.com>
2023-09-01 13:31:45 +0300

    Initial

---
+++ Module 2-3.py
@@ -0,0 +1,5 @@
+l = float(input("What is the length of the rectangle? \n"))
+w = float(input("What is the width of the rectangle? \n"))
+peri = l * 2 + w * 2
+area = l * w
+print("The perimeter of the rectangle is {}, the area of the rectangle is {}.".format(peri, area))
