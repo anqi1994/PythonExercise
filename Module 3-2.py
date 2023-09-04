3b7c7f77777c25a15a577f9e46742296597202a5
An Qi <a1n14q17i9@gmail.com>
2023-09-04 10:23:19 +0300

    Initial

---
+++ Module 3-2.py
@@ -0,0 +1,11 @@
+cc = input("Please enter your cabin class as follows:\nLUX, A, B, or C\nCabin Class: ")
+if cc == "LUX":
+    print("Upper deck cabin with a balcony.")
+elif cc == "A":
+    print("Above the car deck, equipped with a window.")
+elif cc == "B":
+    print("Windowless cabin above the car deck.")
+elif cc == "C":
+    print("Windowless cabin below the car deck.")
+else:
+    print("Invalid cabin class")
