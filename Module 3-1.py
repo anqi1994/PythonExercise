8c53358b2d0c4360ad84eeef9285357c9030b38d
An Qi <a1n14q17i9@gmail.com>
2023-09-04 10:13:40 +0300

    Initial

---
+++ Module 3-1.py
@@ -0,0 +1,5 @@
+leng = float(input("Enter the length of the zander in centimeters:\n"))
+if leng >= 42:
+    print("Congrats, you may keep the zander.")
+else:
+    print("Sorry, you must release the zander.\nThe zander must be at least 42 centimeters long to meet the size limit.")
