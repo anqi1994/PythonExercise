cc8db7fbe73fe0637a64fcb8594d024e17621e6b
An Qi <a1n14q17i9@gmail.com>
2023-09-12 13:35:38 +0300

    Initial

---
+++ Module 4/Module 4-6.py
@@ -0,0 +1,20 @@
+import random
+randf1 = random.uniform(0, 1)
+randf2 = random.uniform(0,1)
+n = 0
+n_all = 0
+ran1 = []
+ran2 = []
+while True:
+    if n_all < 10000000:
+        if randf1 ** 2 + randf2 ** 2 < 1:
+            n = n + 1
+            n_all = n_all + 1
+        else:
+            n_all = n_all + 1
+        randf1 = random.uniform(0, 1)
+        randf2 = random.uniform(0,1)
+    else:
+        break
+pie = 4 * n / n_all
+print(f"The value of pi is about {pie}.")
