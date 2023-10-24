e232cb3da7cf27a3dbc84a2ab36ddcce7fb105db
An Qi <a1n14q17i9@gmail.com>
2023-10-25 00:48:24 +0300

    Initial

---
+++ Module 10/Module10_1.py
@@ -0,0 +1,24 @@
+class Elevator:
+    def __init__(self, bottomfloor, topfloor):
+        self.bottomfloor = bottomfloor
+        self.topfloor = topfloor
+
+    def floor_up(self):
+        self.bottomfloor += 1
+
+    def floor_down(self):
+        self.bottomfloor -= 1
+
+    def go_to_floor(self, floor):
+        self.floor = floor
+        while self.floor > self.bottomfloor:
+            self.floor_up()
+            print(self.bottomfloor)
+        while self.floor < self.bottomfloor:
+            self.floor_down()
+            print(self.bottomfloor)
+
+
+h = Elevator(2, 9)
+h.go_to_floor(7)
+
