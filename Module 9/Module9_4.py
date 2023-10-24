c9fc091746f51d2a5fe95da6c37717ed29bf055b
An Qi <a1n14q17i9@gmail.com>
2023-10-24 23:21:07 +0300

    Initial

---
+++ Module 9/Module9_4.py
@@ -0,0 +1,39 @@
+import random
+class Car:
+    def __init__(self, regisnum, maxsp=0, cursp=0, tradis=0):
+        self.regisnum = regisnum
+        self.maxsp = maxsp
+        self.cursp = cursp
+        self.tradis = tradis
+
+    def accelerate(self, change):
+        self.change = change
+        if change + self.cursp >= self.maxsp:
+            self.cursp = self.maxsp
+        elif change + self.cursp <= 0:
+            self.cursp = 0
+        else:
+            self.cursp = self.cursp + change
+
+    def drive(self, hours):
+        self.hours = hours
+        self.tradis = self.tradis + self.cursp * hours
+
+carlist = []
+carnum = 0
+while carnum < 10:
+    carnum += 1
+    newcar = Car(f"ABC-{carnum}", random.randint(100, 200))
+    carlist.append(newcar)
+
+drivegoal = False
+while drivegoal == False:
+    for car in carlist:
+        car.accelerate(random.randint(-10, 15))
+        car.drive(1)
+        if car.tradis >= 10000:
+            drivegoal = True
+
+for car in carlist:
+    print(car.regisnum, car.maxsp, car.tradis)
+
